b9129099d9362eb142fd0ffbd166e6f2b68aefe3
Юсуф <itrexus69@gmail.com>
2025-11-07 00:18:03 +0300

    1 webhook commit2

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-
+/// https://myawesomewebhook.loca.lt/webhook/github
 app.use('/webhooks', webhookRoutes);
 //app.use('/notifications', notificationRoutes);
 
